f32c9bd477c99aff3feaa7d3cc156010cb0486a2
Ariadne Conill <ariadne@dereferenced.org>
2020-12-30 01:51:20 -0700

    add layer-shell implementation

--- src/library/include/QtWaylandShellHelpers/qlayershellwindow.h
+++ src/library/include/QtWaylandShellHelpers/qlayershellwindow.h
@@ -13,11 +13,72 @@
 #ifndef QTWAYLANDSHELLHELPERS_QLAYERSHELLWINDOW_H
 #define QTWAYLANDSHELLHELPERS_QLAYERSHELLWINDOW_H
 
+#include <cstdint>
 #include <QApplication>
+#include <QWidget>
 
 #include <wayland-client.h>
 
+#include <QtWaylandShellHelpers/qwaylandshellapplication.h>
+
+struct wl_output;
+struct wl_surface;
+struct xdg_popup;
+struct zwlr_layer_shell_v1;
+struct zwlr_layer_surface_v1;
+
 namespace QtWaylandShellHelpers {
+
+class QLayerShellWindow : public QWidget
+{
+public:
+    enum class Layer {
+        Background = 0,
+        Bottom = 1,
+        Top = 2,
+        Overlay = 3,
+    };
+
+    enum class Anchor {
+        None = 0,
+        Top = 1,
+        Bottom = 2,
+        Left = 4,
+        Right = 8,
+    };
+
+    QLayerShellWindow(QWidget *parent = nullptr);
+    ~QLayerShellWindow();
+
+    void showEvent(QShowEvent *event);
+    void resizeEvent(QResizeEvent *event);
+
+    void setLayer(Layer layer);
+    void setAnchor(Anchor anchor);
+    void setExclusiveZone(int32_t zone);
+    void setMargins(int32_t top, int32_t right, int32_t bottom, int32_t left);
+    void setKeyboardInteractivity(bool interactivity);
+
+    static void wlSurfaceConfigureEvent(void *data, ::zwlr_layer_surface_v1 *layer, uint32_t serial, uint32_t width, uint32_t height);
+    static void wlSurfaceCloseEvent(void *data, ::zwlr_layer_surface_v1 *layer);
+
+private:
+    void getSurfaces();
+    void commit();
+
+    QWaylandShellApplication *mApp = nullptr;
+
+    ::wl_surface *mSurface = nullptr;
+    ::zwlr_layer_surface_v1 *mLayerSurface = nullptr;
+    Layer mLayer = Layer::Background;
+    Anchor mAnchor = Anchor::None;
+    int32_t mExclusiveZone = 0;
+    int32_t mMarginTop = 0;
+    int32_t mMarginRight = 0;
+    int32_t mMarginBottom = 0;
+    int32_t mMarginLeft = 0;
+    bool mKeyboardInteractivity = false;
+};
 
 }
 
---
+++ src/library/qlayershellwindow.cc
@@ -0,0 +1,152 @@
+/*
+ * Copyright (c) 2020 Ariadne Conill <ariadne@dereferenced.org>
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * This software is provided 'as is' and without any warranty, express or
+ * implied.  In no event shall the authors be liable for any damages arising
+ * from the use of this software.
+ */
+
+#include <QDebug>
+#include <QtWaylandShellHelpers/qlayershellwindow.h>
+#include <qpa/qplatformnativeinterface.h>
+#include <wlr-layer-shell-unstable-v1-client-protocol.h>
+
+using namespace QtWaylandShellHelpers;
+
+QLayerShellWindow::QLayerShellWindow(QWidget *parent)
+    : QWidget(parent)
+{
+	mApp = qobject_cast<QWaylandShellApplication *>(qApp);
+	if (mApp == nullptr)
+	{
+		qDebug() << "Attempting to make QLayerShellWindow without QWaylandShellApplication is not supported";
+		abort();
+	}
+
+	// XXX: this totally breaks when CSD is actually needed
+	setWindowFlags(Qt::FramelessWindowHint);
+}
+
+QLayerShellWindow::~QLayerShellWindow()
+{
+}
+
+void
+QLayerShellWindow::getSurfaces()
+{
+	auto native = mApp->platformNativeInterface();
+	auto layerShell = mApp->zwlr_layer_shell_v1();
+
+	if (mSurface == nullptr)
+		mSurface = static_cast<::wl_surface *>(native->nativeResourceForWindow("surface", windowHandle()));
+
+	if (mLayerSurface == nullptr)
+	{
+		mLayerSurface = ::zwlr_layer_shell_v1_get_layer_surface(layerShell, mSurface, NULL, (uint32_t) mLayer, "qt-wayland-shell-helpers");
+
+		static ::zwlr_layer_surface_v1_listener surface_listener = {
+			.configure = QLayerShellWindow::wlSurfaceConfigureEvent,
+			.closed = QLayerShellWindow::wlSurfaceCloseEvent,
+		};
+
+		::zwlr_layer_surface_v1_add_listener(mLayerSurface, &surface_listener, this);
+		commit();
+	}
+}
+
+void
+QLayerShellWindow::wlSurfaceConfigureEvent(void *data, ::zwlr_layer_surface_v1 *surface, uint32_t serial, uint32_t width, uint32_t height)
+{
+	auto win = static_cast<QLayerShellWindow *>(data);
+
+	if (!width || !height)
+		return;
+
+	win->resize(width, height);
+	::zwlr_layer_surface_v1_ack_configure(win->mLayerSurface, serial);
+}
+
+void
+QLayerShellWindow::wlSurfaceCloseEvent(void *data, ::zwlr_layer_surface_v1 *surface)
+{
+	auto win = static_cast<QLayerShellWindow *>(data);
+
+	win->close();
+}
+
+void
+QLayerShellWindow::commit()
+{
+	if (mLayerSurface == nullptr)
+		return;
+
+	::zwlr_layer_surface_v1_set_layer(mLayerSurface, (uint32_t) mLayer);
+	::zwlr_layer_surface_v1_set_anchor(mLayerSurface, (uint32_t) mAnchor);
+	::zwlr_layer_surface_v1_set_exclusive_zone(mLayerSurface, mExclusiveZone);
+	::zwlr_layer_surface_v1_set_margin(mLayerSurface, mMarginTop, mMarginRight, mMarginBottom, mMarginLeft);
+	::zwlr_layer_surface_v1_set_keyboard_interactivity(mLayerSurface, (uint32_t) mKeyboardInteractivity);
+
+	::wl_surface_commit(mSurface);
+	::wl_display_roundtrip(mApp->display());
+}
+
+void
+QLayerShellWindow::resizeEvent(QResizeEvent *event)
+{
+	getSurfaces();
+
+	::zwlr_layer_surface_v1_set_size(mLayerSurface, width(), height());
+	commit();
+
+	QWidget::resizeEvent(event);
+}
+
+void
+QLayerShellWindow::showEvent(QShowEvent *event)
+{
+	getSurfaces();
+
+	QWidget::showEvent(event);
+}
+
+void
+QLayerShellWindow::setLayer(Layer layer)
+{
+	mLayer = layer;
+	commit();
+}
+
+void
+QLayerShellWindow::setAnchor(Anchor anchor)
+{
+	mAnchor = anchor;
+	commit();
+}
+
+void
+QLayerShellWindow::setExclusiveZone(int32_t zone)
+{
+	mExclusiveZone = zone;
+	commit();
+}
+
+void
+QLayerShellWindow::setMargins(int32_t top, int32_t right, int32_t bottom, int32_t left)
+{
+	mMarginTop = top;
+	mMarginRight = right;
+	mMarginBottom = bottom;
+	mMarginLeft = left;
+	commit();
+}
+
+void
+QLayerShellWindow::setKeyboardInteractivity(bool interactivity)
+{
+	mKeyboardInteractivity = interactivity;
+	commit();
+}
